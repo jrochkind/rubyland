2b83e518a48553e5029d4894500d9326c1c33a8a
Jonathan Rochkind <jonathan@dnil.net>
2016-11-01 20:16:38 -0400

    limit to only some entries on page

--- app/controllers/aggregator_controller.rb
+++ app/controllers/aggregator_controller.rb
@@ -6,7 +6,7 @@ class AggregatorController < ApplicationController
   protected
 
   def entries
-    @entries ||= Entry.includes(:feed).order("datetime desc").all
+    @entries ||= Entry.includes(:feed).order("datetime desc").where("updated_at > ?", 3.months.ago).limit(110)
   end
   helper_method :entries
 
