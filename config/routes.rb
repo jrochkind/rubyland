f224ba6e7d624030e9864dbf80e711068dc09509
Jonathan Rochkind <jonathan@dnil.net>
2016-11-04 17:31:34 -0400

    placeholder about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "aggregator#index"
 
+  get "/about", to: "aggregator#about", as: :about
+
   get "/sources", to: "feed#index", as: :sources
 end
