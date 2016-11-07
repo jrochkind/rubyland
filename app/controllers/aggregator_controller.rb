99e41d2812feac8c4b8d24d8c21143868b966574
Jonathan Rochkind <jonathan@dnil.net>
2016-11-07 18:27:57 -0500

    fetch entries condition truncated to day for cacheability

--- app/controllers/aggregator_controller.rb
+++ app/controllers/aggregator_controller.rb
@@ -6,7 +6,10 @@ class AggregatorController < ApplicationController
   protected
 
   def entries
-    @entries ||= Entry.includes(:feed).order("datetime desc").where("updated_at > ?", 3.months.ago).limit(110)
+    # start_of_day keeps our condition from changing every second,
+    # for cacheability. 
+    earliest_time = Time.now.utc.beginning_of_day - 3.months
+    @entries ||= Entry.includes(:feed).order("datetime desc").where("updated_at > ?", earliest_time).limit(110)
   end
   helper_method :entries
 
