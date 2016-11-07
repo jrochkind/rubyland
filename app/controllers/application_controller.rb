9987454a2c19f65351a9368d18897d6947607b92
Jonathan Rochkind <jonathan@dnil.net>
2016-11-07 18:29:05 -0500

    demo_password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # just a simple thing to keep google and such away before
+  # we've released.
+  if ENV['DEMO_PASSWORD']
+    before_action :protected_demo_authentication
+  end
+
+  protected
+
+  def protected_demo_authentication
+    auth = authenticate_with_http_basic do |u, p|
+      p == ENV['DEMO_PASSWORD']
+    end
+    if !auth
+      request_http_basic_authentication
+    end
+  end
+
 end
