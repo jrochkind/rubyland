9a18da03ec796748546c5f5ab8b283cebef3aa6b
Jonathan Rochkind <jonathan@dnil.net>
2016-11-07 19:51:23 -0500

    etag caching on home page

--- app/controllers/aggregator_controller.rb
+++ app/controllers/aggregator_controller.rb
@@ -1,13 +1,15 @@
 class AggregatorController < ApplicationController
   def index
-
+    if Rails.env.production? || ENV['HTTP_CACHING']
+      fresh_when etag: etag
+    end
   end
 
   protected
 
   def entries
     # start_of_day keeps our condition from changing every second,
-    # for cacheability. 
+    # for cacheability.
     earliest_time = Time.now.utc.beginning_of_day - 3.months
     @entries ||= Entry.includes(:feed).order("datetime desc").where("updated_at > ?", earliest_time).limit(110)
   end
@@ -17,5 +19,21 @@ class AggregatorController < ApplicationController
     !! params[:titles_only]
   end
   helper_method :titles_only?
+
+  def view_cache_key
+    # cache so the view doesn't have to look it up again, although
+    # I think entries.cache_key would prob be cached anyway, but not certain.
+    @view_cache_key ||= [titles_only?, entries.cache_key]
+  end
+  helper_method :view_cache_key
+
+  def etag
+    # add the heroku SOURCE_VERSION to our etag, so we get
+    # a new etag on every deploy. We could try to access the way
+    # Rails does template dependency fingerprinting, but it misses
+    # a lot of things, and involves kind of reverse engineering Rails.
+    # This is pretty decent.
+    [ENV['SOURCE_VERSION']] + view_cache_key.unshift
+  end
 
 end
