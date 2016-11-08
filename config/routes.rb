6a72a0851c50b186adf2bb37aafd01043d891952
Jonathan Rochkind <jonathan@dnil.net>
2016-11-07 19:01:51 -0500

    add missing route for titles_only

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "aggregator#index"
 
+  get "/titles", to: "aggregator#index", as: :titles_only, defaults: { titles_only: true }
+
   get "/about", to: "aggregator#about", as: :about
 
   get "/sources", to: "feed#index", as: :sources
