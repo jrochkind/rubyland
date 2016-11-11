02e3d030b2a4a64823c46b76ba167340fc848e6c
Jonathan Rochkind <jonathan@dnil.net>
2016-11-11 13:41:02 -0500

    cache headers for static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,11 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.public_file_server.headers = {
+    'Cache-Control' => 'public, s-maxage=31536000, maxage=15552000',
+    'Expires' => "#{1.year.from_now.to_formatted_s(:rfc822)}"
+}
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
