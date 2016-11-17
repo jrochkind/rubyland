16277d1f6843343954fb431e6e136545c58c2d7b
Jonathan Rochkind <jonathan@dnil.net>
2016-11-17 01:04:16 -0500

    order tie-breaker

--- app/controllers/aggregator_controller.rb
+++ app/controllers/aggregator_controller.rb
@@ -11,7 +11,7 @@ class AggregatorController < ApplicationController
     # start_of_day keeps our condition from changing every second,
     # for cacheability.
     earliest_time = Time.now.utc.beginning_of_day - 3.months
-    @entries ||= Entry.includes(:feed).order("datetime desc").where("updated_at > ?", earliest_time).limit(110)
+    @entries ||= Entry.includes(:feed).order("datetime desc, created_at desc").where("updated_at > ?", earliest_time).limit(110)
   end
   helper_method :entries
 
