10f5a2fa353a39f47376a18dde43a10acab9f347
Jonathan Rochkind <jonathan@dnil.net>
2016-11-22 23:09:07 -0500

    better caching in feed controller

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,12 +1,30 @@
 class FeedController < ApplicationController
   def index
-    @feeds = Feed.with_latest_entry.order("UPPER(feeds.title)").all
+    if Rails.env.production? || ENV['HTTP_CACHING']
+      fresh_when etag: etag
+    end
   end
 
   protected
 
   def feeds
-    @feeds
+    @feeds ||= Feed.with_latest_entry.order("UPPER(feeds.title)")
   end
   helper_method :feeds
+
+  def view_cache_key
+    # cache so the view doesn't have to look it up again, although
+    # I think entries.cache_key would prob be cached anyway, but not certain.
+    @view_cache_key ||= [request.format, feeds.cache_key]
+  end
+  helper_method :view_cache_key
+
+  def etag
+    # add the heroku SOURCE_VERSION to our etag, so we get
+    # a new etag on every deploy. We could try to access the way
+    # Rails does template dependency fingerprinting, but it misses
+    # a lot of things, and involves kind of reverse engineering Rails.
+    # This is pretty decent.
+    [ENV['SOURCE_VERSION']] + view_cache_key
+  end  
 end
