764dc924414376a34c98eef8e279b1d1ce42e849
Jonathan Rochkind <jonathan@dnil.net>
2016-11-27 21:17:21 -0500

    user min password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   include Clearance::User
 
+  validates_length_of :password, minimum: 8, except: -> { skip_password_validation? }
+
   def admin?
     role == "admin"
   end
