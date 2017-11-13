333b652bb50986bf542cb3df17e6ec7c2b4751ab
Jonathan Rochkind <jonathan@dnil.net>
2017-11-12 19:16:17 -0500

    remove now unused letsencrypt middleware

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,4 @@
 Rails.application.configure do
-  config.middleware.use Letsencrypt::Middleware
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
